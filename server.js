cb0b355ba14056ee776637bc15b7f742c05523f5
Daniel Barnes <danielbarnes175@gmail.com>
2020-12-19 21:09:23 -0600

    Revert previous commit

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ routes(app);
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    //res.status(err.status || 500);
+    res.status(err.status || 500);
     res.render('404.hbs', {
       message: err.message,
       error: err
@@ -56,7 +56,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  //res.status(err.status || 500);
+  res.status(err.status || 500);
   res.render('404.hbs', {
     message: err.message,
     error: {}

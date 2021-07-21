a33b7e7aea8cae43fc28220aeb207584fa56ff32
Daniel Barnes <danielbarnes175@gmail.com>
2021-07-20 23:17:28 -0700

    Fix logging for 404s

--- api/controller.js
+++ api/controller.js
@@ -31,7 +31,7 @@ const controllers = {
         res.render('videos.hbs', {title: 'Videos', condition: false, videos: videoList});
     },
     contact: function(req, res) {
-        let scripts = [{script: 'https://www.google.com/recaptcha/api.js?render=6Le7JK0bAAAAADV3P6ZDDOXJHJImPrefIVghEs_7'}, {script: '/js/onClickRecaptchaHelper.js'}];
+        let scripts = [{script: 'https://www.google.com/recaptcha/api.js?render=6Le7JK0bAAAAADV3P6ZDDOXJHJImPrefIVghEs_7'}];
         if (req.query)
             res.render('contact.hbs', {title: 'Contact', condition: false, attempted: (req.query.attempted == 'true'), result: (req.query.result == 'true'), scripts: scripts});
         else
--- server.js
+++ server.js
@@ -29,6 +29,32 @@ app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 // Logging middleware
 app.use(function(req, res, next) {
+  logging(req, res);
+  next();
+});
+
+const routes = require('./api/routes.js');
+routes(app);
+
+app.use(function(req, res, next){
+  res.status(404).render('404.hbs');
+  logging(req, res);
+});
+
+var options = {
+    key: privateKey,
+    cert: certificate
+};
+
+https.createServer(options, app).listen(443);
+
+// Redirect from http port 80 to https
+var http = require('http');
+http.createServer(app).listen(80);
+
+console.log("Server Running");
+
+const logging = (req, res) => {
   let current_datetime = new Date();
   let formatted_date =
     current_datetime.getFullYear() +
@@ -47,48 +73,4 @@ app.use(function(req, res, next) {
   let status = res.statusCode;
   let log = `[${formatted_date}]: ${status} ${method} ${url}`;
   console.log(log);
-  next();
-});
-
-const routes = require('./api/routes.js');
-
-routes(app);
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('404.hbs', {
-      message: err.message,
-      error: err
-    });
-  });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('404.hbs', {
-    message: err.message,
-    error: {}
-  });
-});
-
-app.use(function(req, res, next){
-  res.status(404).render('404.hbs');
-});
-
-var options = {
-    key: privateKey,
-    cert: certificate
-};
-
-https.createServer(options, app).listen(443);
-
-// Redirect from http port 80 to https
-var http = require('http');
-http.createServer(app).listen(80);
-
-console.log("Server Running");

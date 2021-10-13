32355ae2c5c5c4d808df86f2589060f711eebfdb
Daniel Barnes <danielbarnes175@gmail.com>
2021-10-12 23:50:27 -0700

    Update swup to disable when target=_blank

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-const swup = new Swup();
+const options = {
+    animateHistoryBrowsing: true,
+    linkSelector: 'a[href^="' +
+    window.location.origin +
+    '"]:not([data-no-swup]):not([target="_blank"]), a[href^="/"]:not([data-no-swup]):not([target="_blank"]), a[href^="#"]:not([data-no-swup]):not([target="_blank"])'
+}
+
+const swup = new Swup(options);
